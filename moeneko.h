70b96a39c4a9d4e2ffda464a91a574a1d9bb9d32
HatsuneMona <songmiao39@foxmail.com>
2018-06-09 00:51:48 +0800

    Initial commit

---
+++ moeneko.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+int suiji(int b);//产生  x<b  的随机数x
+int suiji(int a, int b);//产生  a<x<b  范围的随机数x
+int torf();//随机产生2或者4的数
+void displayout(int a[][4], int clean);
+void putin(int(*a)[4], int quantity);
+bool gameoverleft(int(*a)[4]);
+bool gameoverup(int(*a)[4]);
+void up(int(*a)[4]);
+void down(int(*a)[4]);
+void left(int(*a)[4]);
+void right(int(*a)[4]);
+void xtoy(int(*a)[4]);
+void xtox(int(*a)[4]);
+void ytoy(int(*a)[4]);
